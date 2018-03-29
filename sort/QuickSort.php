7ecc0ea0256b63d4a663525af4b2790d2e2fa1fe
ericivan <ericivan047@gmail.com>
2018-03-29 11:36:39 +0800

    quick sort

---
+++ sort/QuickSort.php
@@ -0,0 +1,32 @@
+<?php
+
+function quickSort($array)
+{
+    if(!isset($array[1]))
+        return $array;
+    $mid = $array[0]; //获取一个用于分割的关键字，一般是首个元素
+    $leftArray = array();
+    $rightArray = array();
+
+    foreach($array as $v)
+    {
+        if($v > $mid)
+            $rightArray[] = $v;  //把比$mid大的数放到一个数组里
+        if($v < $mid)
+            $leftArray[] = $v;   //把比$mid小的数放到另一个数组里
+    }
+
+    $leftArray = quickSort($leftArray); //把比较小的数组再一次进行分割
+    $leftArray[] = $mid;        //把分割的元素加到小的数组后面，不能忘了它哦
+
+    $rightArray = quickSort($rightArray);  //把比较大的数组再一次进行分割
+    return array_merge($leftArray,$rightArray);  //组合两个结果
+}
+
+$arr = [5, 1, 6, 7, 4, 3, 77, 33];
+
+$result = quickSort($arr);
+//var_dump(1);
+print_r($result);
+
+
